86098b3aae92fe3cfe5b6ed1e28a016778a3d661
DevOwl <c.muehle18@googlemail.com>
2015-06-01 20:54:19 +0200

    Added function to get next free id Changed string and &str handling Added test method to main for dev testing

--- rNote/src/main.rs
+++ rNote/src/main.rs
@@ -4,10 +4,35 @@ extern crate time;
 mod setting;
 mod notes;
 use notes::Note;
+use std::env;
 
 
 fn main() {
-   let settings = setting::get_config();
-   let mut my_note = notes::Note::new();
-   println!("{}", my_note.creation);
+   tests();   
+}
+
+fn tests(){
+	println!("");
+	println!("-----------------Self test-----------------------------");
+	print!("Get config: ");
+	let settings = setting::get_config();
+	println!("OK");
+	println!("Creating new note");
+	print!("1. New id: ");
+	let new_id: u32 = notes::Note::new_id( &settings.get_default("data","data"), 
+										   settings.get_default("id_offset","500").parse::<u32>().unwrap());
+	println!("{}", new_id);
+	print!("Note struct: ");
+	let mut my_note = notes::Note::new(new_id,"Owls everywhere");
+	println!("OK");
+	print!("Add a tag: ");
+	my_note.add_tag("Testing");
+	println!("OK");
+	println!("Our note so far:");
+	println!("ID          -> {}", my_note.id);
+	println!("title       -> {}", my_note.title);
+	println!("tag         -> {:?}", my_note.tag);
+	println!("creation    -> {}", my_note.creation);
+	println!("last_update -> {}", my_note.last_update);
+	println!("-------------------------------------------------------");
 }
--- rNote/src/notes.rs
+++ rNote/src/notes.rs
@@ -4,38 +4,57 @@ use std::fs;
 use std::path::Path;
 
 #[derive(Default)]
-pub struct Note{
-	pub title: String,
-	pub tag: Vec<String>,
+pub struct Note<'a>{
+	pub title: &'a str,
+	pub tag: Vec<&'a str>,
 	pub last_update: u32,
 	pub creation: u32,
 	pub id: u32,
 }
 
 
-impl Note{
+impl<'a> Note<'a>{
 	
-	pub fn new(new_id : u32,  new_title: String) -> Note{
+	pub fn new(new_id : u32,  new_title: &str) -> Note{
 		Note {creation: unix_timestamp(), id:new_id,title:new_title, ..Default::default() }
 	}
 	
-	pub fn add_tag(&mut self, tag: &str){
-		if !self.tag.contains( &tag.to_string()){
-			self.tag.push( tag.to_string());
+	pub fn add_tag(&mut self, tag: &'a str){
+		if !self.has_tag(tag){
+			self.tag.push(tag);
 		}
+		self.update();
 	}
 	
 	pub fn has_tag(&self, tag: &str) ->bool{
-		self.tag.contains( &tag.to_string())
+		self.tag.contains( &tag)
 	}
 	
 	pub fn update(&mut self){
 		self.last_update = unix_timestamp();
 	}
 	
-	pub fn new_id(path: String, id_offset: u32) -> u32{
+	pub fn new_id(path: &str, id_offset: u32) -> u32{
+		let mut new_id: u32 = 0;
+		let mut tmp : u32 = id_offset;
 		//Get list of files search for next free id
-		1
+		match file_list( Path::new( path)){
+			Ok(v) => {
+					  for x in v.iter() 
+					  {
+						if match x.parse::<u32>() {
+							Ok(v) => v,
+							Error => 0,
+						} > tmp {
+							tmp = x.parse::<u32>().unwrap();
+						}
+					  } 
+					},
+			Error => panic!("Unable to read in data directory at {}", path),
+		};
+		
+		new_id += tmp +1;
+		new_id
 	}
 	
 }
@@ -51,7 +70,7 @@ pub fn file_list(dir: &Path) -> io::Result<Vec<String>>{
 		let entry = try!(entry);
 		let meta  = try!(fs::metadata( entry.path()));
 		if meta.is_file() {
-			files.push( entry.path().to_str().unwrap().to_string());
+			files.push( entry.path().file_name().unwrap().to_str().unwrap().to_string());
 		}
 	}
     Ok(files)
